f022430c76d7076cf51888816324a08017e953c5
RRoman202 <romanrrr2005rrr@gmail.com>
2024-03-16 20:36:36 +0500

    asdasd

---
+++ Controllers/NotificationsController.cs
@@ -0,0 +1,40 @@
+﻿using Demo.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Controllers
+{
+    public class NotificationsController : Controller
+    {
+
+        private readonly DataContext _context;
+
+        public NotificationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "1")]
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Notifications.Include(a => a.User).Include(a => a.Application);
+            return View(await dataContext.ToListAsync());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var noti = await _context.Notifications.FindAsync(id);
+            if (noti != null)
+            {
+                _context.Notifications.Remove(noti);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index", "Notifications");
+        }
+    }
+}
